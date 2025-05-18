98de5e4058e3c421e27fabcc9e02a09e2efb7de3
gg <georgevanderveen@yahoo.fr>
2025-05-18 13:08:54 +0200

    correction orthographique

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 PIECES = {
     "knight": {
-        "mooves": (
+        "moves": (
             (2, 1),
             (2, -1),
             (-2, 1),
@@ -12,13 +12,13 @@ PIECES = {
         ),
         "repeat": False,
     },
-    "rook": {"mooves": ((1, 0), (0, 1), (-1, 0), (0, -1)), "repeat": True},
-    "king": {"mooves": ((1, 0), (0, 1), (-1, 0), (0, -1)), "repeat": False},
-    "pawnw": {"mooves": (1, 0), "repeat": False},
-    "pawnb": {"mooves": (-1, 0), "repeat": False},
-    "bihsop": {"mooves": ((1, 1), (-1, -1), (1, -1), (-1, 1)), "repeat": True},
+    "rook": {"moves": ((1, 0), (0, 1), (-1, 0), (0, -1)), "repeat": True},
+    "king": {"moves": ((1, 0), (0, 1), (-1, 0), (0, -1)), "repeat": False},
+    "pawnw": {"moves": (1, 0), "repeat": False},
+    "pawnb": {"moves": (-1, 0), "repeat": False},
+    "bihsop": {"moves": ((1, 1), (-1, -1), (1, -1), (-1, 1)), "repeat": True},
     "queen": {
-        "mooves": (
+        "moves": (
             (1, 0),
             (0, 1),
             (-1, 0),
@@ -28,7 +28,7 @@ PIECES = {
             (1, -1),
             (-1, 1),
         ),
-        "reapeat": True,
+        "repeat": True,
     },
 }
 
@@ -37,26 +37,26 @@ board = [
     [
         ("rook", "b"),
         ("knight", "b"),
-        ("bishop", "b"),
+        ("bihsop", "b"),
         ("queen", "b"),
         ("king", "b"),
-        ("bishop", "b"),
+        ("bihsop", "b"),
         ("knight", "b"),
         ("rook", "b"),
     ],
     [("pawnb", "b") for i in range(8)],
-    ["0" for i in range(8)],
-    ["0" for i in range(8)],
-    ["0" for i in range(8)],
-    ["0" for i in range(8)],
+    ["00" for i in range(8)],
+    ["00" for i in range(8)],
+    ["00" for i in range(8)],
+    ["00" for i in range(8)],
     [("pawnw", "w") for i in range(8)],
     [
         ("rook", "w"),
         ("knight", "w"),
-        ("bishop", "w"),
+        ("bihsop", "w"),
         ("king", "w"),
         ("queen", "w"),
-        ("bishop", "w"),
+        ("bihsop", "w"),
         ("knight", "w"),
         ("rook", "w"),
     ],
@@ -64,51 +64,50 @@ board = [
 
 
 def print_board(board):
-    for line in board:
+    for i, line in enumerate(board):
+        print(i, end=" ")
         for cell in line:
             print(cell[0][0], end=" ")
         print()
 
 
 def get_piece(board, cell):
+    # TODO : case vide
     return board[cell[0]][cell[1]]
 
 
-print(get_piece(board, (0, 3)))
-
-
 def list_valid_move(board, cell) -> list:
-    piece_type = get_piece(cell)[0]
-    piece_color = get_piece(cell)[1]
+    piece_type = get_piece(board, cell)[0]
+    piece_color = get_piece(board, cell)[1]
     move_list = []
-    for move in PIECES[piece_type]["mooves"]:
+    for move in PIECES[piece_type]["moves"]:
         actual_position = cell
         if PIECES[piece_type]["repeat"]:
             while True:
                 actualy = actual_position[0] + move[0]
                 actualx = actual_position[1] + move[1]
                 if 0 <= actualy <= 7 and 0 <= actualx <= 7:
-                    if get_piece((actualy, actualx))[1] != piece_color:
+                    if get_piece(board, (actualy, actualx))[1] != piece_color:
                         move_list.append((actualy, actualx))
                         actual_position = (actualy, actualx)
-                        if board[actualy, actualx] != 0:
+                        # TODO : MOCHE CHANGER !
+                        if board[actualy, actualx] != "00":
                             break
                 else:
                     break
         else:
             actualy = actual_position[0] + move[0]
             actualx = actual_position[1] + move[1]
-            if 0 <= actualy <= 9 and 0 <= actualx <= 9:
-                if get_piece((actualy, actualx))[1] != piece_color:
-                    move_list.append(move)
+            if 0 <= actualy <= 7 and 0 <= actualx <= 7:
+                if get_piece(board, (actualy, actualx))[1] != piece_color:
+                    move_list.append((actualy, actualx))
+    return move_list
 
 
-def moove_piece(board): ...
-
-
-def main():
-    print_board(board)
+def move_piece(board): ...
 
 
 if __name__ == "__main__":
-    main()
+    board[3][3] = ("knight", "w")
+    print(list_valid_move(board, (3, 3)))
+    print_board(board)
