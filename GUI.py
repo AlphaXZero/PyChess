25bd1142dcc2b555cc8df299187a907a328fd2d4
gg <georgevanderveen@yahoo.fr>
2025-05-29 20:50:39 +0200

    themes added

--- GUI.py
+++ GUI.py
@@ -2,13 +2,84 @@ import ttkbootstrap as tk
 import engine
 from tkinter import messagebox
 
+COLOR_CHOICE = "Forest"
+# Themes made by @cindy.vanderveen
 COLOR_THEME = {
-    "white": "white",
-    "black": "gray14",
-    "black_board": "DarkOliveGreen3",
-    "white_board": "Antique white",
+    "Classic": {
+        "white": {"fill": "#f2f2f2", "line": "black"},
+        "black": {"fill": "#464646", "line": "black"},
+        "black_board": "#777777",
+        "white_board": "#ffffff",
+    },
+    "High On Drugs": {
+        "white": {"fill": "#00ff38", "line": "#00f9ff"},
+        "black": {"fill": "#3c00ff", "line": "#00f9ff"},
+        "black_board": "#fd00ff",
+        "white_board": "#fdff00",
+    },
+    "Halloween": {
+        "white": {"fill": "#00ba91", "line": "#00ba91"},
+        "black": {"fill": "#863174", "line": "#863174"},
+        "black_board": "black",
+        "white_board": "#e26b0f",
+    },
+    "Holy Blood": {
+        "black": {"fill": "red4", "line": "red4"},
+        "white": {"fill": "indian red", "line": "brown4"},
+        "black_board": "red",
+        "white_board": "tomato",
+    },
+    "Neon": {
+        "white": {"fill": "cyan", "line": "white"},
+        "black": {"fill": "deep pink", "line": "white"},
+        "white_board": "CadetBlue1",
+        "black_board": "magenta2",
+    },
+    "Wood": {
+        "white": {"fill": "ivory2", "line": "black"},
+        "black": {"fill": "black", "line": "black"},
+        "white_board": "NavajoWhite2",
+        "black_board": "saddle brown",
+    },
+    "Chess.com": {
+        "white": {"fill": "#ffffff", "line": "#4b4847"},
+        "black": {"fill": "#4b4847", "line": "#4b4847"},
+        "white_board": "#eeeed2",
+        "black_board": "#69923e",
+    },
+    "Star Wars": {
+        "white": {"fill": "RoyalBlue1", "line": "RoyalBlue1"},
+        "black": {"fill": "red3", "line": "red3"},
+        "white_board": "navy",
+        "black_board": "black",
+    },
+    "Royal": {
+        "white": {"fill": "gold", "line": "dark green"},
+        "black": {"fill": "purple4", "line": "red4"},
+        "white_board": "red4",
+        "black_board": "blue4",
+    },
+    "Noel": {
+        "white": {"fill": "#f4f0bb", "line": "#f4f0bb"},
+        "black": {"fill": "#43291f", "line": "#43291f"},
+        "white_board": "#87c38f",
+        "black_board": "#da2c38",
+    },
+    "Dusk": {
+        "white": {"fill": "#7C4585", "line": "#7C4585"},
+        "black": {"fill": "#F8B55F", "line": "#F8B55F"},
+        "white_board": "#C95792",
+        "black_board": "#3D365C",
+    },
+    "Army": {
+        "white": {"fill": "#25591f", "line": "#25591f"},
+        "black": {"fill": "#593a0e", "line": "#593a0e"},
+        "white_board": "#72601b",
+        "black_board": "#19270d",
+    },
 }
 
+LINE_SETTINGS = {"width": 3}
 current_board = engine.board
 
 color = ["white", "black"]
@@ -82,7 +153,7 @@ def draw_grid():
                     SIZE * y,
                     SIZE + (SIZE * i),
                     SIZE + (SIZE * y),
-                    fill=COLOR_THEME["white_board"],
+                    fill=COLOR_THEME[COLOR_CHOICE]["white_board"],
                 )
             else:
                 # noir
@@ -91,7 +162,7 @@ def draw_grid():
                     SIZE * y,
                     SIZE + (SIZE * i),
                     SIZE + (SIZE * y),
-                    fill=COLOR_THEME["black_board"],
+                    fill=COLOR_THEME[COLOR_CHOICE]["black_board"],
                 )
 
 
@@ -174,7 +245,7 @@ def draw_help_circles(coords):
         )
 
 
-def draw_knight(x, y, color):
+def draw_knight(x, y, settings):
     global canvas
     x, y = x * SIZE, y * SIZE
     # rectangle nez
@@ -183,8 +254,7 @@ def draw_knight(x, y, color):
         y + gp(40),
         x + gp(90),
         y + gp(60),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # triangle nez
     canvas.create_polygon(
@@ -194,8 +264,7 @@ def draw_knight(x, y, color):
         y + gp(40),
         x + gp(70),
         y + gp(20),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # rectangle corps
     canvas.create_rectangle(
@@ -203,8 +272,7 @@ def draw_knight(x, y, color):
         y + gp(20),
         x + gp(90),
         y + gp(90),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # base
     canvas.create_rectangle(
@@ -212,8 +280,7 @@ def draw_knight(x, y, color):
         y + gp(80),
         x + gp(90),
         y + gp(90),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # triangle pied
     canvas.create_polygon(
@@ -223,8 +290,7 @@ def draw_knight(x, y, color):
         y + gp(60),
         x + gp(70),
         y + gp(80),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # rectanle oreille
     canvas.create_rectangle(
@@ -232,41 +298,47 @@ def draw_knight(x, y, color):
         y + gp(10),
         x + gp(80),
         y + gp(20),
-        outline=color,
-        fill=color,
+        **settings,
     )
+    # rectangle oreille
+    canvas.create_line(x + gp(60), y + gp(10), x + gp(60), y + gp(24), **LINE_SETTINGS)
+    canvas.create_line(x + gp(80), y + gp(10), x + gp(80), y + gp(20), **LINE_SETTINGS)
+    canvas.create_line(x + gp(60), y + gp(10), x + gp(80), y + gp(10), **LINE_SETTINGS)
+    # ligne droite oreille
+    canvas.create_line(x + gp(80), y + gp(20), x + gp(90), y + gp(20), **LINE_SETTINGS)
+    # corps ligne vertical
+    canvas.create_line(x + gp(90), y + gp(20), x + gp(90), y + gp(90), **LINE_SETTINGS)
+    # pied
+    canvas.create_line(x + gp(40), y + gp(90), x + gp(90), y + gp(90), **LINE_SETTINGS)
+    # pied gauche
+    canvas.create_line(x + gp(40), y + gp(80), x + gp(40), y + gp(90), **LINE_SETTINGS)
+    # hyppotenuse pied
+    canvas.create_line(x + gp(40), y + gp(80), x + gp(60), y + gp(65), **LINE_SETTINGS)
+    # mileu gauche
+    canvas.create_line(x + gp(60), y + gp(66), x + gp(60), y + gp(62), **LINE_SETTINGS)
+    # mileu horizontal
+    canvas.create_line(x + gp(20), y + gp(62), x + gp(60), y + gp(62), **LINE_SETTINGS)
+    # nez vertical
+    canvas.create_line(x + gp(20), y + gp(62), x + gp(20), y + gp(40), **LINE_SETTINGS)
+    # nez hyppotenuse
+    canvas.create_line(x + gp(20), y + gp(40), x + gp(60), y + gp(23), **LINE_SETTINGS)
+
     # cercle oeil
-    # TODO CHANEGER couleur
-    canvas.create_oval(
-        x + gp(60),
-        y + gp(30),
-        x + gp(70),
-        y + gp(40),
-        outline=color,
-        fill="white" if color == "black" else "black",
-    )
+    settings["fill"] = "black"
+    canvas.create_oval(x + gp(60), y + gp(30), x + gp(70), y + gp(40), **settings)
 
 
-def draw_rook(x, y, color):
+def draw_rook(x, y, settings):
     global canvas
     x, y = x * SIZE, y * SIZE
-    # base
-    canvas.create_rectangle(
-        x + gp(20),
-        y + gp(70),
-        x + gp(80),
-        y + gp(90),
-        outline=color,
-        fill=color,
-    )
+
     # rectanngle corps
     canvas.create_rectangle(
         x + gp(30),
         y + gp(40),
         x + gp(70),
         y + gp(80),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # rectangle sommet
     canvas.create_rectangle(
@@ -274,8 +346,7 @@ def draw_rook(x, y, color):
         y + gp(20),
         x + gp(76),
         y + gp(40),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # petit rectangles sommets
     canvas.create_rectangle(
@@ -283,57 +354,59 @@ def draw_rook(x, y, color):
         y + gp(10),
         x + gp(34),
         y + gp(20),
-        outline=color,
-        fill=color,
+        **settings,
     )
     canvas.create_rectangle(
         x + gp(44),
         y + gp(10),
         x + gp(56),
         y + gp(20),
-        outline=color,
-        fill=color,
+        **settings,
     )
     canvas.create_rectangle(
         x + gp(66),
         y + gp(10),
         x + gp(76),
         y + gp(20),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # base
-    canvas.create_line(x + gp(20), y + gp(70), x + gp(20), y + gp(90), width=2)
-    canvas.create_line(x + gp(20), y + gp(90), x + gp(80), y + gp(90), width=2)
-    canvas.create_line(x + gp(80), y + gp(70), x + gp(80), y + gp(90), width=2)
+    settings.update({"width": LINE_SETTINGS["width"], "outline": LINE_SETTINGS["fill"]})
+    canvas.create_rectangle(
+        x + gp(20),
+        y + gp(70),
+        x + gp(80),
+        y + gp(90),
+        **settings,
+    )
     # petites lignes base
-    canvas.create_line(x + gp(20), y + gp(70), x + gp(30), y + gp(70), width=2)
-    canvas.create_line(x + gp(80), y + gp(70), x + gp(70), y + gp(70), width=2)
+    canvas.create_line(x + gp(20), y + gp(70), x + gp(30), y + gp(70), **LINE_SETTINGS)
+    canvas.create_line(x + gp(80), y + gp(70), x + gp(70), y + gp(70), **LINE_SETTINGS)
     # corps
-    canvas.create_line(x + gp(30), y + gp(70), x + gp(30), y + gp(40), width=2)
-    canvas.create_line(x + gp(70), y + gp(70), x + gp(70), y + gp(40), width=2)
+    canvas.create_line(x + gp(30), y + gp(70), x + gp(30), y + gp(40), **LINE_SETTINGS)
+    canvas.create_line(x + gp(70), y + gp(70), x + gp(70), y + gp(40), **LINE_SETTINGS)
     # petutes lignes bas ehaut
-    canvas.create_line(x + gp(24), y + gp(40), x + gp(30), y + gp(40), width=2)
-    canvas.create_line(x + gp(76), y + gp(40), x + gp(70), y + gp(40), width=2)
+    canvas.create_line(x + gp(24), y + gp(40), x + gp(76), y + gp(40), **LINE_SETTINGS)
+    canvas.create_line(x + gp(76), y + gp(40), x + gp(70), y + gp(40), **LINE_SETTINGS)
     # haut cotés
-    canvas.create_line(x + gp(24), y + gp(40), x + gp(24), y + gp(10), width=2)
-    canvas.create_line(x + gp(76), y + gp(40), x + gp(76), y + gp(10), width=2)
+    canvas.create_line(x + gp(24), y + gp(40), x + gp(24), y + gp(10), **LINE_SETTINGS)
+    canvas.create_line(x + gp(76), y + gp(40), x + gp(76), y + gp(10), **LINE_SETTINGS)
     # petites lignes sommet
-    canvas.create_line(x + gp(24), y + gp(10), x + gp(34), y + gp(10), width=2)
-    canvas.create_line(x + gp(44), y + gp(10), x + gp(56), y + gp(10), width=2)
-    canvas.create_line(x + gp(66), y + gp(10), x + gp(76), y + gp(10), width=2)
+    canvas.create_line(x + gp(24), y + gp(10), x + gp(34), y + gp(10), **LINE_SETTINGS)
+    canvas.create_line(x + gp(44), y + gp(10), x + gp(56), y + gp(10), **LINE_SETTINGS)
+    canvas.create_line(x + gp(66), y + gp(10), x + gp(76), y + gp(10), **LINE_SETTINGS)
     # petites lignes sommet bas
-    canvas.create_line(x + gp(34), y + gp(20), x + gp(44), y + gp(20), width=2)
-    canvas.create_line(x + gp(66), y + gp(20), x + gp(56), y + gp(20), width=2)
+    canvas.create_line(x + gp(34), y + gp(20), x + gp(44), y + gp(20), **LINE_SETTINGS)
+    canvas.create_line(x + gp(66), y + gp(20), x + gp(56), y + gp(20), **LINE_SETTINGS)
     # peties lignes verticales
-    canvas.create_line(x + gp(24), y + gp(10), x + gp(24), y + gp(20), width=2)
-    canvas.create_line(x + gp(34), y + gp(10), x + gp(34), y + gp(20), width=2)
-    canvas.create_line(x + gp(44), y + gp(10), x + gp(44), y + gp(20), width=2)
-    canvas.create_line(x + gp(56), y + gp(10), x + gp(56), y + gp(20), width=2)
-    canvas.create_line(x + gp(66), y + gp(10), x + gp(66), y + gp(20), width=2)
+    canvas.create_line(x + gp(24), y + gp(10), x + gp(24), y + gp(20), **LINE_SETTINGS)
+    canvas.create_line(x + gp(34), y + gp(10), x + gp(34), y + gp(20), **LINE_SETTINGS)
+    canvas.create_line(x + gp(44), y + gp(10), x + gp(44), y + gp(20), **LINE_SETTINGS)
+    canvas.create_line(x + gp(56), y + gp(10), x + gp(56), y + gp(20), **LINE_SETTINGS)
+    canvas.create_line(x + gp(66), y + gp(10), x + gp(66), y + gp(20), **LINE_SETTINGS)
 
 
-def draw_pawn(x, y, color):
+def draw_pawn(x, y, settings):
     global canvas
     x, y = x * SIZE, y * SIZE
     # base
@@ -342,8 +415,7 @@ def draw_pawn(x, y, color):
         y + gp(70),
         x + gp(80),
         y + gp(90),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # trapeze corps
     canvas.create_polygon(
@@ -355,49 +427,38 @@ def draw_pawn(x, y, color):
         y + gp(50),
         x + gp(70),
         y + gp(70),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # rectangle sommet
-    canvas.create_rectangle(
-        x + gp(34),
-        y + gp(30),
-        x + gp(66),
-        y + gp(50),
-        outline=color,
-        fill=color,
-    )
+    settings.update({"width": LINE_SETTINGS["width"], "outline": LINE_SETTINGS["fill"]})
+    canvas.create_rectangle(x + gp(34), y + gp(30), x + gp(66), y + gp(50), **settings)
     # rond sommmet
-    canvas.create_oval(
-        x + gp(40),
-        y + gp(10),
-        x + gp(60),
-        y + gp(30),
-        outline=color,
-        fill=color,
-    )
+
+    canvas.create_oval(x + gp(40), y + gp(10), x + gp(60), y + gp(30), **settings)
+    # base
+    canvas.create_line(x + gp(20), y + gp(90), x + gp(80), y + gp(90), **LINE_SETTINGS)
+    canvas.create_line(x + gp(20), y + gp(90), x + gp(20), y + gp(70), **LINE_SETTINGS)
+    canvas.create_line(x + gp(80), y + gp(90), x + gp(80), y + gp(70), **LINE_SETTINGS)
+    # base haut
+    canvas.create_line(x + gp(80), y + gp(70), x + gp(70), y + gp(70), **LINE_SETTINGS)
+    canvas.create_line(x + gp(20), y + gp(70), x + gp(30), y + gp(70), **LINE_SETTINGS)
+    # triangle coté
+    canvas.create_line(x + gp(30), y + gp(70), x + gp(40), y + gp(50), **LINE_SETTINGS)
+    canvas.create_line(x + gp(70), y + gp(70), x + gp(60), y + gp(50), **LINE_SETTINGS)
 
 
-def draw_bishop(x, y, color):
+def draw_bishop(x, y, settings):
     global canvas
     x, y = x * SIZE, y * SIZE
-    # base
-    canvas.create_rectangle(
-        x + gp(20),
-        y + gp(70),
-        x + gp(80),
-        y + gp(90),
-        outline=color,
-        fill=color,
-    )
+    settings.update({"width": LINE_SETTINGS["width"], "outline": LINE_SETTINGS["fill"]})
+
     # ovale corps
     canvas.create_oval(
-        x + gp(40),
+        x + gp(35),
         y + gp(20),
-        x + gp(60),
+        x + gp(65),
         y + gp(80),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # rond sommmet
     canvas.create_oval(
@@ -405,12 +466,19 @@ def draw_bishop(x, y, color):
         y + gp(10),
         x + gp(54),
         y + gp(20),
-        outline=color,
-        fill=color,
+        **settings,
+    )
+    # base
+    canvas.create_rectangle(
+        x + gp(25),
+        y + gp(75),
+        x + gp(75),
+        y + gp(90),
+        **settings,
     )
 
 
-def draw_queen(x, y, color):
+def draw_queen(x, y, settings):
     global canvas
     x, y = x * SIZE, y * SIZE
     # base
@@ -419,8 +487,7 @@ def draw_queen(x, y, color):
         y + gp(80),
         x + gp(90),
         y + gp(90),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # grand triangle
     canvas.create_polygon(
@@ -430,9 +497,9 @@ def draw_queen(x, y, color):
         y + gp(80),
         x + gp(50),
         y + gp(40),
-        outline=color,
-        fill=color,
+        **settings,
     )
+    settings.update({"width": LINE_SETTINGS["width"], "outline": LINE_SETTINGS["fill"]})
     # petit triangle
     canvas.create_polygon(
         x + gp(50),
@@ -441,21 +508,29 @@ def draw_queen(x, y, color):
         y + gp(20),
         x + gp(70),
         y + gp(20),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # rond sommet
+
     canvas.create_oval(
         x + gp(46),
         y + gp(10),
         x + gp(54),
         y + gp(20),
-        outline=color,
-        fill=color,
+        **settings,
     )
+    # base
+    canvas.create_line(x + gp(10), y + gp(90), x + gp(90), y + gp(90), **LINE_SETTINGS)
+    canvas.create_line(x + gp(10), y + gp(90), x + gp(10), y + gp(80), **LINE_SETTINGS)
+    canvas.create_line(x + gp(90), y + gp(90), x + gp(90), y + gp(80), **LINE_SETTINGS)
+    canvas.create_line(x + gp(10), y + gp(80), x + gp(20), y + gp(80), **LINE_SETTINGS)
+    canvas.create_line(x + gp(90), y + gp(80), x + gp(80), y + gp(80), **LINE_SETTINGS)
+    # trianle bas
+    canvas.create_line(x + gp(20), y + gp(80), x + gp(50), y + gp(44), **LINE_SETTINGS)
+    canvas.create_line(x + gp(80), y + gp(80), x + gp(50), y + gp(44), **LINE_SETTINGS)
 
 
-def draw_king(x, y, color):
+def draw_king(x, y, settings):
     global canvas
     x, y = x * SIZE, y * SIZE
     # base
@@ -464,8 +539,7 @@ def draw_king(x, y, color):
         y + gp(80),
         x + gp(90),
         y + gp(90),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # traèze
     canvas.create_polygon(
@@ -477,27 +551,36 @@ def draw_king(x, y, color):
         y + gp(50),
         x + gp(30),
         y + gp(50),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # sommet vertical
+    settings.update({"width": LINE_SETTINGS["width"], "outline": LINE_SETTINGS["fill"]})
     canvas.create_rectangle(
         x + gp(46),
         y + gp(50),
         x + gp(54),
         y + gp(10),
-        outline=color,
-        fill=color,
+        **settings,
     )
     # sommet horizontal
+
     canvas.create_rectangle(
         x + gp(28),
         y + gp(24),
         x + gp(72),
         y + gp(28),
-        outline=color,
-        fill=color,
+        **settings,
     )
+    # base
+    canvas.create_line(x + gp(10), y + gp(90), x + gp(90), y + gp(90), **LINE_SETTINGS)
+    canvas.create_line(x + gp(10), y + gp(90), x + gp(10), y + gp(80), **LINE_SETTINGS)
+    canvas.create_line(x + gp(90), y + gp(90), x + gp(90), y + gp(80), **LINE_SETTINGS)
+    canvas.create_line(x + gp(10), y + gp(80), x + gp(20), y + gp(80), **LINE_SETTINGS)
+    canvas.create_line(x + gp(90), y + gp(80), x + gp(80), y + gp(80), **LINE_SETTINGS)
+    # trapèze
+    canvas.create_line(x + gp(20), y + gp(80), x + gp(30), y + gp(50), **LINE_SETTINGS)
+    canvas.create_line(x + gp(80), y + gp(80), x + gp(70), y + gp(50), **LINE_SETTINGS)
+    canvas.create_line(x + gp(30), y + gp(50), x + gp(70), y + gp(50), **LINE_SETTINGS)
 
 
 def draw_board():
@@ -507,20 +590,27 @@ def draw_board():
 
 
 def draw_piece(piece, x, y):
+    global DRAW_SETTINGS
+    if piece[0] != "0":
+        DRAW_SETTINGS = {
+            "outline": COLOR_THEME[COLOR_CHOICE][piece[1]]["fill"],
+            "fill": COLOR_THEME[COLOR_CHOICE][piece[1]]["fill"],
+        }
+        LINE_SETTINGS.update({"fill": COLOR_THEME[COLOR_CHOICE][piece[1]]["line"]})
     if piece[0] == "bishop":
-        draw_bishop(x, y, COLOR_THEME[piece[1]])
+        draw_bishop(x, y, DRAW_SETTINGS)
     if piece[0] == "rook":
-        draw_rook(x, y, COLOR_THEME[piece[1]])
+        draw_rook(x, y, DRAW_SETTINGS)
     if piece[0] == "pawnb":
-        draw_pawn(x, y, COLOR_THEME[piece[1]])
+        draw_pawn(x, y, DRAW_SETTINGS)
     if piece[0] == "pawnw":
-        draw_pawn(x, y, COLOR_THEME[piece[1]])
+        draw_pawn(x, y, DRAW_SETTINGS)
     if piece[0] == "knight":
-        draw_knight(x, y, COLOR_THEME[piece[1]])
+        draw_knight(x, y, DRAW_SETTINGS)
     if piece[0] == "queen":
-        draw_queen(x, y, COLOR_THEME[piece[1]])
+        draw_queen(x, y, DRAW_SETTINGS)
     if piece[0] == "king":
-        draw_king(x, y, COLOR_THEME[piece[1]])
+        draw_king(x, y, DRAW_SETTINGS)
 
 
 def show_move(y, x, newy, newx, col):
