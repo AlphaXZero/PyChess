55b877ddba5520d1f64958b7224292befebe2e01
gg <georgevanderveen@yahoo.fr>
2025-06-12 15:09:57 +0200

    end new boar

--- game.py
+++ game.py
@@ -1,11 +1,5 @@
 from board.board import Board
-from pieces.queen import Queen
-from pieces.rook import Rook
-from pieces.pawn import Pawn
-from pieces.king import King
-from pieces.bishop import Bishop
-from pieces.knight import Knight
-from pieces.piece import Piece
+
 
 if __name__ == "__main__":
     board = Board()
